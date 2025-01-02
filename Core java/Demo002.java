302533e05d252901701b842c480699a879c8f103
Tharuni18 <tharunisai2006@gmail.com>
2025-01-02 15:06:13 +0530

    finding greatest of two numbers

---
+++ Core java/Demo002.java
@@ -0,0 +1,16 @@
+public class Demo2{
+    public static void main(String args[]){
+        int a;
+        int b;
+        int a=30;
+        int b=15;
+
+        if(a>b){
+            System.out.println(a+ " is greatest");
+        }
+        else{
+            System.out.println(b+ " is greatest");
+        }
+
+    }
+}
