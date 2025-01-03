552864019e14e0062dff26d69e4bfb1f62d2aeae
Tharuni18 <tharunisai2006@gmail.com>
2025-01-03 20:28:07 +0530

    Checking User Credentials

---
+++ Core java/Demo006.java
@@ -0,0 +1,14 @@
+public class Demo006 {
+    public static void main(String args[]) {
+        String username = "Tharuni";
+        String pwd = "Tharuni@123";
+        
+        while ((username == "Tharuni") && (pwd == "Tharuni@123")){
+            System.out.println("Hello Tharuni! Welcome ");
+            break;
+
+        }
+       
+        
+    }
+}
