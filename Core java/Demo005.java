43cd7b624ab6ae142c336fa44fb763a1385a73b9
Tharuni18 <tharunisai2006@gmail.com>
2025-01-03 09:53:18 +0530

    Checking Credentials using while loop

---
+++ Core java/Demo005.java
@@ -0,0 +1,13 @@
+// username = user001 pwd = user_001
+
+class Demo005{
+    public static void main(String args[]){
+        String username = "user001";
+        String pwd = "user_001";
+        
+        while((username == "username") && (pwd == "user_001")){
+            Sytem.out.println("Hello! Happy New Year");
+            break;
+        }
+    }
+}
