79e63f4de1c563c7acd3a241ba63cb6ea6d27a4d
Tharuni18 <tharunisai2006@gmail.com>
2025-01-20 14:02:36 +0530

    Array Example

---
+++ Core java/Demo029.java
@@ -0,0 +1,17 @@
+public class Demo029{
+    public static void main(String[] args) {
+        String[] arr = new String[10];
+        arr[0] = "Tharuni";
+        arr[1] = "kavita";
+        arr[2] = "teju";
+        arr[3] = "harsha";
+        arr[4] = "shine";
+        arr[5] = "tulja";
+        arr[6] = "vysh";
+        arr[7] = "jhansi";
+        arr[8] = "anu";
+        arr[9] = "priyanka";
+
+        System.out.println(arr[9]);
+    }
+}
