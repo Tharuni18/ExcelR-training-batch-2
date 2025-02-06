4b3668df9c69af8ffe0806d431c76e6ecff03128
Tharuni18 <tharunisai2006@gmail.com>
2025-02-06 09:54:34 +0530

    To print all array elements using loop

---
+++ Core java/Demo065.java
@@ -0,0 +1,11 @@
+public class Demo065 {
+    public static void main(String[] args) {
+        String[] arr = new String[]{"Om","Ash","pruthvi","Nibisha","vrishkapi"};
+        System.out.println("Elements of array are:");
+        for(int i = 0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+        
+    
+}
